2f6b101e1aa0d991636e5970f16d2c167d7e339e
MH <mike.hall.0316@gmail.com>
2024-01-20 23:57:04 -0600

    friday night practice baby

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -160,3 +160,29 @@ else
     Console.WriteLine("Found Chuck");
 */
 
+
+//Code challenge: update problematic code in the code editor
+
+int[] numbers = { 4, 8, 15, 16, 23, 42 };
+
+foreach (int number in numbers)
+{
+    int total;
+
+    total += number;
+
+    if (number == 42)
+    {
+        bool found = true;
+
+    }
+
+}
+
+if (found)
+{
+    Console.WriteLine("Set contains 42");
+
+}
+
+Console.WriteLine($"Total: {total}");
