32f177a3b8af13f7527e7c587ea0e3731bde8189
MH <mike.hall.0316@gmail.com>
2024-02-14 20:45:16 -0600

    dowhile pracitce - 02.14.24

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -466,3 +466,24 @@ Console.WriteLine($"Product: {size} {color} {type}");
 //        Console.WriteLine(x);
 //    }
 //}
+
+////Write a while statement that iterates only while a random number is greater than some value
+
+//Random random = new Random();
+//int current = random.Next(1, 11);
+
+///*
+//do
+//{
+//    current = random.Next(1, 11);
+//    Console.WriteLine(current);
+//} while (current != 7);
+//*/
+
+//while (current >= 3)
+//{
+//    Console.WriteLine(current);
+//    current = random.Next(1, 11);
+//}
+//Console.WriteLine($"Last number: {current}");
+
